fca928bbd0b775cc78b2100b92cb560111b22e12
ChoiSangwon <fdsa200@naver.com>
2024-10-07 14:27:35 +0900

    feat : add 생성자

---
+++ First/src/classpart/PersonTest.java
@@ -0,0 +1,9 @@
+package classpart;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person("choi");
+        Person person3 = new Person("AAA", 2.3F, 234.3F);
+    }
+}
